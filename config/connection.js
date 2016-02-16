6c488b08522bb3d5f6c561dd9c7d4ee32a311599
evan bates <evan.bates@rutgers.edu>
2016-02-16 17:32:45 -0500

    pw cdb

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");
 var connection = mysql.createConnection({
-port		 : '3306',
-host     : '',
-user     : 'root',
-password : '',
+port		 : '',
+host     : 'us-cdbr-iron-east-03.cleardb.net/heroku_3b830081a93023d?reconnect=true',
+user     : 'bc162d348b0b12',
+password : 'a520eb56',
 database : 'veggieburger'
 });
 
