382c128f39d3b3cac6d072b9ae191f92d4fd0cb2
evan bates <evan.bates@rutgers.edu>
2016-02-16 12:49:16 -0500

    herk13

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 var connection = mysql.createConnection({
 port		 : '3306',
-host     : 'localhost',
+host     : '',
 user     : 'root',
 password : '',
 database : 'veggieburger'
