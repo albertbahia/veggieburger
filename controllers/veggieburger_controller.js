e2e4898e69e5b5cd9d163d4f5fa6ef3447fd58f9
evan bates <evan.bates@rutgers.edu>
2016-02-16 17:01:18 -0500

    herk 13

--- controllers/veggieburger_controller.js
+++ controllers/veggieburger_controller.js
@@ -3,29 +3,29 @@ var veggieburger = require("../models/veggieburger.js");
 var router = express.Router(); 
 router.get("/", function(req, res) {
 	veggieburger.findWhole(function(getsomewholeresults){
-	  console.log("next line is getsomeresults");
-	veggieburger.findEaten(function(getsomeeatenresults){
-	  console.log("next line is getsomeresults");
-		res.render('template1', {getsomeeatenresults, getsomewholeresults});
+		console.log("next line is getsomeresults");
+		veggieburger.findEaten(function(getsomeeatenresults){
+			console.log("next line is getsomeresults");
+			res.render('template1', {getsomeeatenresults, getsomewholeresults});
 		});
-});
+	});
 });
 router.post("/", function(req, res) {
 	console.log(req.body.bname);
 	var x = req.body.bname;
 	veggieburger.addBurger(x, function(getsomeresults){
-	console.log(getsomeresults);
-  res.redirect("/");
-		});
+		console.log(getsomeresults);
+		res.redirect("/");
+	});
 });
 
 router.get("/eat/:burger", function(req, res) {
 	console.log(req.params.burger);
 	var x = req.params.burger;
 	veggieburger.eatBurger(x, function(getsomeresults){
-	console.log(getsomeresults);
-  res.redirect("/");
-		});
+		console.log(getsomeresults);
+		res.redirect("/");
+	});
 });
 
 
