99053ba2ea505b92ff5148a8dab6a9e9df3afd23
evan bates <evan.bates@rutgers.edu>
2016-02-17 09:51:41 -0500

    more beaut.

--- models/veggieburger.js
+++ models/veggieburger.js
@@ -1,24 +1,23 @@
 var orm = require("../config/orm.js");
 var burger = {
-	findWhole: function(modelcallback) {
-		orm.selectWhole(function(res) {
-			modelcallback(res);
-		});
-	
-},
-	findEaten: function(modelcallback) {
-		orm.selectEaten(function(res) {
-			modelcallback(res);
-		});
-	
-},
+  findWhole: function(modelcallback) {
+    orm.selectWhole(function(res) {
+      modelcallback(res);
+    });
+  },
 
-	addBurger: function(x, modelcallback) {
-orm.addBurger(x, modelcallback) 
-},	
-	eatBurger: function(x, modelcallback) {
-orm.eatBurger(x, modelcallback) 
-}	
+  findEaten: function(modelcallback) {
+    orm.selectEaten(function(res) {
+      modelcallback(res);
+    });
+  },
 
+  addBurger: function(x, modelcallback) {
+    orm.addBurger(x, modelcallback) 
+  },	
+
+  eatBurger: function(x, modelcallback) {
+    orm.eatBurger(x, modelcallback) 
+  }	
 };
 module.exports = burger;
--- public/js/clickster.js
+++ public/js/clickster.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-$(".btn").on("click", function(){
+  $(".btn").on("click", function(){
     var bid = $(this).attr("id");
-		var url = window.location.pathname + "eat/" +  bid;
-		console.log(url);
-		window.location.replace(url);
-});		
+    var url = window.location.pathname + "eat/" +  bid;
+    console.log(url);
+    window.location.replace(url);
+  });		
 });		
 
 
