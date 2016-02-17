ea146c7ef44bd34b72fb246b7db7a5ebf8b26692
evan bates <evan.bates@rutgers.edu>
2016-02-17 14:27:10 -0500

    jdb

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,12 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+//var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
+var connection = mysql.createConnection({
+  host     : 'tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user     : 'r8nbo7is7sjj69mp',
+  password : 'el7q31tocxldjlgc',
+  database : 'x33whqas8s34mqb9'
+});
 connection.connect();
 
 connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
