58c245df92e1b31366e0df3a85ea1d747464a887
evan bates <evan.bates@rutgers.edu>
2016-02-18 13:58:59 -0500

    test

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,5 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
-connection.connect();
 
 connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
   if (err) throw err;
