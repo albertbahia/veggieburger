90657baf286efd68d61506689a78ca034546eb47
evan bates <evan.bates@rutgers.edu>
2016-02-18 12:18:07 -0500

    hk

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require("express");
 var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
 var routes = require('./controllers/veggieburger_controller.js');
-var PORT = process.env.PORT || 5000;
 
 var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
@@ -10,4 +9,4 @@ app.use(express.static('public'));
 app.use('/', routes);
 app.engine('handlebars', expressHandlebars({defaultLayout: 'mainlayout'}));
 app.set("view engine", "handlebars");
-app.listen(PORT);
+app.listen(process.env.PORT || 3000);
