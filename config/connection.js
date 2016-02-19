a016b152007f6895e6fa63b542f04b7baeff4b0f
evan bates <evan.bates@rutgers.edu>
2016-02-19 11:40:59 -0500

    hk

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,16 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
+  port: 3306,
   host: 'tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   user: 'uaax750h569k46ex',
    password: 'bi40472bvwxs93oe',
    database: 'xgiyu6ba2wzk2zzb'
 });
 
-connection.connect();
 connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
   if (err) throw err;
 
 console.log('The solution is: ', rows[0].solution);
 });
-connection.end();
 
 module.exports = connection;
