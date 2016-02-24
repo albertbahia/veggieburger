cd1f95759a7c909f9b694127ad23c9ed431aaac6
evan bates <evan.bates@rutgers.edu>
2016-02-24 10:09:02 -0500

    jaws

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,11 @@
+var settings = require('./settings.js');
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-  port: 3306,
-  host: 'tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user: 'uaax750h569k46ex',
-   password: 'bi40472bvwxs93oe',
-   database: 'xgiyu6ba2wzk2zzb'
-});
-
+var connection = mysql.createConnection(settings.JAWSDB_URL);
+connection.connect();
 connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
   if (err) throw err;
 
 console.log('The solution is: ', rows[0].solution);
 });
-
+connection.end();
 module.exports = connection;
---
+++ config/settings.js
@@ -0,0 +1,1 @@
+exports.JAWSDB_URL = 'mysql://uaax750h569k46ex:bi40472bvwxs93oe@tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/xgiyu6ba2wzk2zzb'
