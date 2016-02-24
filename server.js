c06acd6c499fae28ef085c46e4efb1ffc01af999
evan bates <evan.bates@rutgers.edu>
2016-02-24 12:22:31 -0500

    hk

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var envs = require('dotenv').config();;
 var express = require("express");
-var port = process.env.PORT || CONFIG.port;
+var port = process.env.PORT || 3000;
 var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
 var routes = require('./controllers/veggieburger_controller.js');
