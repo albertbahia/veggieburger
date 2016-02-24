169edb87180964ff6c74302e23dcd61eff9fb731
evan bates <evan.bates@rutgers.edu>
2016-02-24 12:02:23 -0500

    hj

--- config/connection.js
+++ config/connection.js
@@ -2,5 +2,6 @@ var mysql = require('mysql');
 console.log(process.env.JAWSDB_URL);
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
 console.log("connection to db");
+console.log(connection);
 connection.connect();
 module.exports = connection;
