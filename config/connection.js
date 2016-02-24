a0bfac1ff35b40fe6a575114f742dc7ba8cf89b5
evan bates <evan.bates@rutgers.edu>
2016-02-24 10:52:54 -0500

    hk

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,6 @@
 var mysql = require('mysql');
+console.log(process.env.JAWSDB_URL);
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
+console.log("connection to db");
 connection.connect();
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-console.log('The solution is: ', rows[0].solution);
-});
-connection.end();
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-var envs = require('dotenv');
+var envs = require('dotenv').config();
 var express = require("express");
+var PORT = process.env.NODE_ENV || 9000;  
 var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
 var routes = require('./controllers/veggieburger_controller.js');
@@ -10,4 +11,4 @@ app.use('/', routes);
 app.engine('handlebars', expressHandlebars({defaultLayout: 'mainlayout'}));
 app.set("view engine", "handlebars");
 
-app.listen(process.env.NODE_ENV  || 9000);
+app.listen(PORT);
